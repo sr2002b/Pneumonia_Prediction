145610c5a28d234fba71e98b315dfcd1e7ff2eaa
Samuel Rosinsky <sr2002b@hsutx.edu>
2023-04-30 15:49:55 -0500

    Added comments to make the code easier to understand.

--- DecisionTree.py
+++ DecisionTree.py
@@ -8,8 +8,9 @@ from sklearn.metrics import classification_report, confusion_matrix, accuracy_sc
 import pandas as pd
 import time
 
-# Load the dataset
+# set the dataset path to the location of the chest_xray folder
 dataset_path = 'chest_xray'
+# set the train, val, and test paths as subfolders of the dataset path
 train_path = os.path.join(dataset_path, 'train')
 val_path = os.path.join(dataset_path, 'val')
 test_path = os.path.join(dataset_path, 'test')
@@ -17,24 +18,36 @@ test_path = os.path.join(dataset_path, 'test')
 
 # Preprocess the data
 def preprocess_data(data_path, img_size=(150, 150)):
+    # create empty lists for images and labels
     images = []
     labels = []
+    # for each label in the dataset (NORMAL and PNEUMONIA)
     for label in ['NORMAL', 'PNEUMONIA']:
+        # set the label path to the location of the label folder
         label_path = os.path.join(data_path, label)
+        # for each image in the label folder
         for img_name in os.listdir(label_path):
+            # set the image path to the location of the image
             img_path = os.path.join(label_path, img_name)
+            # open the image and convert to grayscale
             img = Image.open(img_path).convert('L')  # Convert to grayscale
+            # resize the image to the specified size
             img = img.resize(img_size)
+            # convert the image to a numpy array and normalize the values
             img = np.array(img) / 255.0
+            # append the image
             images.append(img)
+            # append the label and if the label is NORMAL, set it to 0, otherwise set it to 1
             labels.append(0 if label == 'NORMAL' else 1)
+    # return the images and labels as numpy arrays
     return np.array(images), np.array(labels)
 
-
+# set train images, val images, and test images  to the images and labels returned by preprocess_data
 train_images, train_labels = preprocess_data(train_path)
 val_images, val_labels = preprocess_data(val_path)
 test_images, test_labels = preprocess_data(test_path)
 
+# set train images, val images, and test images to the images reshaped to 150 * 150
 train_images = train_images.reshape(-1, 150 * 150)
 val_images = val_images.reshape(-1, 150 * 150)
 test_images = test_images.reshape(-1, 150 * 150)
@@ -42,8 +55,9 @@ test_images = test_images.reshape(-1, 150 * 150)
 # Start the timer
 start_time = time.time()
 
-# Train the Decision Tree classifier
+# Create the Decision Tree Classifier
 dt_classifier = DecisionTreeClassifier()
+# Fit the model
 dt_classifier.fit(train_images, train_labels)
 
 # Stop the timer
